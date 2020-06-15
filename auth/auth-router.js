ea54b12b59a7adfba78a50b50d5615a790f3a8ec
SFMack <sfcamack@icloud.com>
2020-06-14 18:41:31 -0700

    register function implemented

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+const bcrypt = require('bcryptjs');
+
+const Users = require('../auth/auth-model.js');
+
+
+// create new user
+router.post('/register', async (req, res) => {
+  const newUser = req.body;
+  const hashedPassword = bcrypt.hashSync(newUser.password, 12);
+  newUser.password = hashedPassword;
+
+  try {
+    const saved = await Users.addUser(newUser);
+    res.status(201).json({ message: `User ${newUser.username} created`})
+  } catch (error) {
+    console.log(error);
+    res.status(404).json(error);
+  }
+
 });
 
 router.post('/login', (req, res) => {
