bc2ec7427fc9cb3ce692ce6dc130dbdb1d7c6085
SFMack <sfcamack@icloud.com>
2020-06-14 18:51:00 -0700

    login functionality implemented

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,8 +21,21 @@ router.post('/register', async (req, res) => {
 
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res) => {
+  let { username, password } = req.body;
+
+  try {
+    const foundUser = await Users.findUser({username}).first();
+    if(foundUser && bcrypt.compareSync(password, foundUser.password)) {
+      // req.session.user = foundUser;
+      res.status(200).json({ message: `Welcome, ${foundUser.username}...`})
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' })
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
